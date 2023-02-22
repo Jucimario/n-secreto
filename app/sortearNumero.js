3834d79c40fba1a59f7c1148a299b4f2a0f49341
jucimario <jucimarioo@live.com>
2023-02-21 21:55:34 -0300

    projeto finalizado

---
+++ app/sortearNumero.js
@@ -0,0 +1,15 @@
+const elementoMenorValor = document.getElementById('menor-valor');
+const elementoMaiorValor = document.getElementById('maior-valor');
+
+const menorValor = 1;
+const maiorValor = 1000;
+const numeroSecreto = gerarNumeroAleatorio();
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * parseInt(maiorValor + 1));
+}
+
+elementoMenorValor.innerHTML = menorValor;
+elementoMaiorValor.innerHTML = maiorValor;
+
+console.log('Numero Secreto: ' + numeroSecreto);
---
+++ app/validacao.js
@@ -0,0 +1,39 @@
+function verificaSeOChuteEstaCorreto(chute) {
+    const numero = + chute;
+
+    if (chuteForInvalido(numero)) {
+        elementoChute.innerHTML += `<div>Valor Inválido</div>`;
+        return;
+    }
+    if (numeroForMaiorQueOValorPermitido(numero)) {
+
+        elementoChute.innerHTML += `<div>O numero deve estar entre ${menorValor} e ${maiorValor}</div>`;
+        return;
+    }
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `<h2>Voce acertou!</h2>
+        <h3> o numero secreto: ${numeroSecreto}</h3>
+        <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
+        `;
+    }
+    else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `<div>O numero secreto é menor <i class="fa-solid fa-down-long"></i></div>`;
+    }
+    else {
+        elementoChute.innerHTML += `<div>O numero secreto é maior <i class="fa-solid fa-up-long"></i></div>`;
+    }
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero);
+}
+
+function numeroForMaiorQueOValorPermitido(numero) {
+    return numero > maiorValor || numero < menorValor;
+}
+
+document.body.addEventListener('click', (event) => {
+    if (event.target.id === 'jogar-novamente') {
+        window.location.reload();
+    }
+})
