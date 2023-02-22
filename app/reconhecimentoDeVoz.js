83fdb11d2c344db1bcfac91e81ba2276ed3ac905
jucimario <jucimarioo@live.com>
2023-02-22 15:05:09 -0300

    Validação de texto e valor maior que 1000

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -11,16 +11,18 @@ recognition.addEventListener('result', onSpeak);
 function onSpeak(event) {
     chute = event.results[0][0].transcript
     exibeChuteNaTela(chute);
-
     verificaSeOChuteEstaCorreto(chute);
 }
 
 function exibeChuteNaTela(chute) {
 
+    if (chuteForInvalido(parseInt(chute))) {
+        return;
+    }
     elementoChute.innerHTML = `
     <div>Voce disse: </div>
     <span class="box">${chute}</span>
-    `
+    `;
 }
 
 recognition.addEventListener('end', () => recognition.start());
